da7796d4e23c5dc5521d83ba555d1d1d6f7a9f82
Yinzhu Su <yinzhusu@gmail.com>
2022-12-15 18:56:38 -0800

    Used Route, added skill icons

--- Yinzhu Project 9/src/components/TechnicalSkills.js
+++ Yinzhu Project 9/src/components/TechnicalSkills.js
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
 import skills from "./jsons/technicalSkills.json";
+import cassandra from "../images/icons/cassandra.png"
+import firebase from "../images/icons/firebase.png"
+import flask from "../images/icons/flask.png"
+import HJC from "../images/icons/HJC.png"
+import java from "../images/icons/java.png"
+import matlab from "../images/icons/matlab.png"
+import mongodb from "../images/icons/mongodb.png"
+import node from "../images/icons/node.png"
+import opencv from "../images/icons/opencv.png"
+import python from "../images/icons/python.png"
+import qt from "../images/icons/qt.png"
+import react from "../images/icons/react.png"
+import scikitlearn from "../images/icons/scikitlearn.png"
+import sqlite from "../images/icons/sqlite.png"
+import swift from "../images/icons/swift.png"
+
 
 const TechnicalSkills = () => {
 
@@ -12,8 +28,25 @@ const TechnicalSkills = () => {
                     <h4>{skill["Database, IDE, and Tools"]}</h4>
                 </div>
             ))
-
         }
+        <div className="icons">
+            <img src={firebase} alt="firebase" className="icon"></img>
+            <img src={flask} alt="flask" className="icon"></img>
+            <img src={HJC} alt="HJC" className="icon"></img>
+            <img src={java} alt="java" className="icon"></img>
+            <img src={matlab} alt="matlab" className="icon"></img>
+            <img src={mongodb} alt="mongodb" className="icon"></img>
+            <img src={cassandra} alt="cassandra" className="icon"></img>
+            <img src={node} alt="node" className="icon"></img>
+            <img src={opencv} alt="opencv" className="icon"></img>
+            <img src={python} alt="python" className="icon"></img>
+            <img src={qt} alt="qt" className="icon"></img>
+            <img src={react} alt="react" className="icon"></img>
+            <img src={scikitlearn} alt="scikitlearn" className="icon"></img>
+            <img src={sqlite} alt="sqlite" className="icon"></img>
+            <img src={swift} alt="swift" className="icon"></img>
+        </div>
+
     </>
 }
 
--- Yinzhu Project 9/src/index.js
+++ Yinzhu Project 9/src/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import selfie from "./images/IMG_1123.JPG"
 import { createRoot } from "react-dom/client"
+
 import "./style.css"
 import {
     Home,
@@ -11,36 +12,64 @@ import {
     ContactInformation,
 } from "./components/indexingHelper.js"
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+
+
 const App = () => {
 
-    const [page, setPage] = useState("Home")
-    const pageHelper = (pageName) => {
-        setPage(pageName);
-    }
+    // const [page, setPage] = useState("Home")
+    // const pageHelper = (pageName) => {
+    //     setPage(pageName);
+    // }
 
     return <>
         <h1> Yinzhu Su </h1>
         <img src={selfie} alt="Yinzhu Su's Image" className="homeImage" />
 
-        <div className="navigitorBar">
+        {/* <div className="navigitorBar">
             <h4 className="indexButton" onClick={() => pageHelper("Home")}>Home</h4>
-            <h4 className="indexButton" onClick={() => pageHelper("Educations")}>Education</h4>
+            <h4 className="indexButton" onClick={() => pageHelper("Educations")}>Educations</h4>
             <h4 className="indexButton" onClick={() => pageHelper("Technical Skills")}>Technical Skills</h4>
             <h4 className="indexButton" onClick={() => pageHelper("Projects")}>Projects</h4>
-            <h4 className="indexButton" onClick={() => pageHelper("Work Experiences")}>Work Experience</h4>
+            <h4 className="indexButton" onClick={() => pageHelper("Work Experiences")}>Work Experiences</h4>
             <h4 className="indexButton" onClick={() => pageHelper("Contact Information")}> Contact Information </h4>
             <a href="https://drive.google.com/file/d/1UJj32YOu1_wrwBjwSe6QgeiFBBbsHTFU/view?usp=sharing" target="_blank"><h4 className="indexButton"> Download My Resume</h4></a>
-        </div>
-
+        </div> */}
+        {/* 
         {(page === "Home") && <Home />}
         {(page === "Contact Information") && <ContactInformation />}
         {(page === "Educations") && <Educations />}
         {(page === "Technical Skills") && <TechnicalSkills />}
         {(page === "Projects") && <Projects />}
-        {(page === "Work Experiences") && <WorkExperiences />}
+        {(page === "Work Experiences") && <WorkExperiences />} */}
+
+        <div className="navigitorBar">
+            <Link to="/home"><h4 className="indexButton"> Home </h4></Link>
+            <Link to="/educations"><h4 className="indexButton"> Educations </h4></Link>
+            <Link to="/skills"><h4 className="indexButton"> Technical Skills </h4></Link>
+            <Link to="/projects"><h4 className="indexButton"> Projects </h4></Link>
+            <Link to="/workexperiences"><h4 className="indexButton"> Work Experiences </h4></Link>
+            <Link to="/contact"> <h4 className="indexButton"> Contact Information </h4> </Link>
+            <a href="https://drive.google.com/file/d/1UJj32YOu1_wrwBjwSe6QgeiFBBbsHTFU/view?usp=sharing" target="_blank"><h4 className="indexButton"> Download My Resume</h4></a>
+        </div>
+
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} />
+            <Route path="educations" element={<Educations />} />
+            <Route path="skills" element={<TechnicalSkills />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="workexperiences" element={<WorkExperiences />} />
+            <Route path="contact" element={<ContactInformation />} />
+        </Routes>
     </>
 }
 
 const app = document.querySelector("#app");
 const root = createRoot(app);
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
